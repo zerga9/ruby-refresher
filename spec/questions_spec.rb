5d2e9c8cd31b244d27d47365e579744ba2e747bf
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-07 17:48:48 +0000

    Adds filepath to text sample

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
 end
 
 describe 'the Friday test :)' do
-  
+
   it 'select_elements_starting_with_a' do
     n = select_elements_starting_with_a ['bananas', 'apples', 'pears', 'avocados']
     expect(n).to eq ['apples', 'avocados']
@@ -217,7 +217,7 @@ describe 'the Friday test :)' do
   end
 
   it 'count_words_of_each_length_in_a_file' do
-    n = count_words_of_each_length_in_a_file('lorem.txt') || []
-    expect(Hash[n.sort]).to eq({1=>1, 2=>5, 3=>7, 4=>12, 5=>14, 6=>4, 7=>8, 8=>6, 9=>6, 10=>2, 11=>2, 12=>3}) 
+    n = count_words_of_each_length_in_a_file('data/lorem.txt') || []
+    expect(Hash[n.sort]).to eq({1=>1, 2=>5, 3=>7, 4=>12, 5=>14, 6=>4, 7=>8, 8=>6, 9=>6, 10=>2, 11=>2, 12=>3})
   end
 end
